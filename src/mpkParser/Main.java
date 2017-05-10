e9e3c8903c69c91a91f5cc5c49c5d3bd7c4eaf4e
mq007 <marcin.slowiak.007@gmail.com>
2017-05-10 13:58:03 +0200

    Added Lane class, setters and getters done, parsing lane numbers from String to List created.

--- src/mpkParser/Main.java
+++ src/mpkParser/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import mpkParser.html.LanesParser;
 
 public class Main extends Application {
 
@@ -21,7 +22,9 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+        LanesParser lp = new LanesParser();
+        lp.getLanesFromSite();
+        //launch(args);
     }
 }
 
--- src/mpkParser/html/LanesParser.java
+++ src/mpkParser/html/LanesParser.java
@@ -1,20 +1,52 @@
 package mpkParser.html;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by MQ on 2017-05-10.
  */
 public class LanesParser {
-    private String url = "http://www.mpk.krakow.pl/pl/page-f3044045/";
+    private String urlAdress = "http://rozklady.mpk.krakow.pl/?lang=PL&rozklad=20170510";
 
-    private String [] tramDay;
-    private String [] tramNight;
-    private String [] busCityDay;
-    private String [] busCityNight;
-    private String [] busAgglomerationDay;
-    private String [] busAgglomerationNight;
-    private String [] busCityAccelerated;
-    private String [] busAgglomerationAccelerated;
-    private String [] busCitySupport;
-    private String [] busCitySubstitute;
+    public void getLanesFromSite(){
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(urlAdress).get();
+            Elements header = doc.select(".t_info_left");
+            Elements data = doc.select(".linia_table_left");
 
+            List<String> headers = new ArrayList<>();
+            List<String> laneNumber = new ArrayList<>();
+
+            String text = "";
+
+            for(Element l : header){
+                text = l.text();
+                text = text.substring(0, text.length()-1);
+                if(!text.contains("Opis")){
+                    headers.add(text);
+                    System.out.println(text);
+                }
+            }
+
+            for(Element l : data){
+                text = l.text();
+                laneNumber.add(text);
+                System.out.println(text);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setTramsAndBusesNumbers(List<String> header, List<String> laneNumber){
+
+    }
 }
