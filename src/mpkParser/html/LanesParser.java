74dc1761e48411a0900ed7dd7f65752687262ce1
mq007 <marcin.slowiak.007@gmail.com>
2017-05-10 12:41:40 +0200

    Added LanesParser class

---
+++ src/mpkParser/html/LanesParser.java
@@ -0,0 +1,20 @@
+package mpkParser.html;
+
+/**
+ * Created by MQ on 2017-05-10.
+ */
+public class LanesParser {
+    private String url = "http://www.mpk.krakow.pl/pl/page-f3044045/";
+
+    private String [] tramDay;
+    private String [] tramNight;
+    private String [] busCityDay;
+    private String [] busCityNight;
+    private String [] busAgglomerationDay;
+    private String [] busAgglomerationNight;
+    private String [] busCityAccelerated;
+    private String [] busAgglomerationAccelerated;
+    private String [] busCitySupport;
+    private String [] busCitySubstitute;
+
+}
